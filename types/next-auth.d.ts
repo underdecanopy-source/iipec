b99d382dbd73aca421b3e05b3e815f484c8ebc68
underdecanopy-source <underdecanopy@gmail.com>
2026-08-03 18:05:57 +0100

    Update next-auth.d.ts

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,29 +1,50 @@
-import type { DefaultSession } from 'next-auth'
-import type { DefaultJWT } from 'next-auth/jwt'
+import { UserRole } from '@prisma/client'
+import NextAuth, { DefaultSession } from 'next-auth'
+import { AdapterUser } from 'next-auth/adapters'
+import { JWT } from 'next-auth/jwt'
 
 declare module 'next-auth' {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
   interface Session {
-    user?: DefaultSession['user'] & {
-      id?: string
-      role?: string
+    user: {
+      id: string
+      role: UserRole
       phone?: string | null
       address?: string | null
-    }
+    } & DefaultSession['user']
   }
 
+  /**
+   * The shape of the user object returned in the OAuth providers' `profile` callback,
+   * or the `authorize` callback for Credentials provider.
+   */
   interface User {
-    id?: string
-    role?: string
+    role: UserRole
     phone?: string | null
     address?: string | null
+    profileImage?: string | null
+  }
+}
+
+declare module 'next-auth/adapters' {
+  interface AdapterUser {
+    role: UserRole
+    phone?: string | null
+    address?: string | null
+    profileImage?: string | null
   }
 }
 
 declare module 'next-auth/jwt' {
-  interface JWT extends DefaultJWT {
-    id?: string
-    role?: string
+  /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
+  interface JWT {
+    id: string
+    role: UserRole
     phone?: string | null
     address?: string | null
+    picture?: string | null
+    name?: string | null
   }
 }
